222488aa1bc132ed5b181a80bb8d0795b8b28062
Farah <meselmenifarah@gmail.com>
2026-06-04 20:32:51 +0100

    rien

--- Program.cs
+++ Program.cs
@@ -24,10 +24,18 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngular", policy =>
     {
-        policy.WithOrigins("http://localhost:4200")
+        
+                policy.WithOrigins(
+                  "http://localhost:4200",
+                  "http://192.168.1.29:4200",
+                  "https://localhost",
+                  "capacitor://localhost",
+                  "http://localhost"
+              )
               .AllowAnyMethod()
               .AllowAnyHeader()
               .AllowCredentials(); // Obligatoire pour SignalR
+
     });
 });
 
